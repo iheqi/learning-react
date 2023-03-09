4d798de0559ad8c501be9d5a30766f9cb3a533ae
何琦 <>
2023-03-09 18:10:41 +0800

    faker.js & react-window

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,6 +2,17 @@ import React, { useState, useEffect, memo } from "react";
 import WordCount from "./components/WordCount";
 import User from "./components/User";
 import Cat from "./components/Cat";
+import List from "./components/List";
+
+import { FixedSizeList } from "react-window";
+
+import { faker } from '@faker-js/faker';
+
+const bigList = [...Array(5000)].map(() => ({
+  name: faker.internet.userName(),
+  email: faker.internet.email(),
+  avatar: faker.image.avatar(),
+}));
 
 // meow函数会导致重新渲染，而第二个参数可以控制是否重新渲染，返回false重新渲染
 const PureCat = memo(Cat, (prevProps, nextProps) => prevProps.name === nextProps.name);
@@ -38,7 +49,28 @@ function App() {
   // }, []);
 
   const [cats, setCats] = useState(["Biscuit", "jungle", "Outlaw"]);
+  const renderItem = item => (
+    <div style={{ display: "flex" }}>
+      <img src={item.avatar} alt={item.name} width={50}></img>
+      <p>
+        {item.name} - {item.email}
+      </p>
+    </div>
+  )
 
+  const renderRow = ({ index, style }) => (
+    <div style={{ ...style, ...{ display: 'flex' } }}>
+      <img
+        src={bigList[index].avatar}
+        alt={bigList[index].name}
+        width={50}
+      />
+
+      <p>
+        {bigList[index].name} - {bigList[index].email}
+      </p>
+    </div>
+  )
   return (
     <div className="App">
       <label>Favorite phrase:</label>
@@ -62,6 +94,18 @@ function App() {
       <button onClick={() => setCats([...cats, prompt("Name a cat")])}>
         Add a cat
       </button>
+
+
+      {/* <List data={bigList} renderItem={renderItem} renderEmpty={<p>This list is empty.</p>}></List> */}
+
+      <FixedSizeList
+        height={window.innerHeight}
+        width={window.innerWidth - 20}
+        itemCount={bigList.length}
+        itemSize={50}
+      >
+        {renderRow}
+      </FixedSizeList>
     </div>
   );
 }
