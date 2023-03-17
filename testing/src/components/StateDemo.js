c500f76b7d7e3dceadec3fc1ab65c604b1c5c3eb
何琦 <>
2023-03-17 13:23:12 +0800

    7-11 setState 合并 state 的情况

--- testing/src/components/StateDemo.js
+++ testing/src/components/StateDemo.js
@@ -17,28 +17,54 @@ export default class StateDemo extends React.Component {
     </div>
   }
 
-  // increase = () => {
-  //   // 第二：不要直接修改state，不可变值
-  //   // this.state.count++; // 无效
-  //   this.setState({ count: this.state.count + 1 }, () => {
-  //     console.log('count by callback', this.state.count); // 可以传递第二个参数，获取更新后的值
-  //   });
+  increase = () => {
+    // 第二：不要直接修改state，不可变值
+    // this.state.count++; // 无效
+    // this.setState({ count: this.state.count + 1 }, () => {
+    //   console.log('count by callback', this.state.count); // 可以传递第二个参数，获取更新后的值
+    // });
 
-  //   // 第三，setState 可能是异步的，也有可能是同步的
-  //   console.log('count', this.state.count);
+    // // 第三，setState 可能是异步的，也有可能是同步的
+    // console.log('count', this.state.count);
 
-  //   // setTimeout 中 setState 是同步的
-  //   setTimeout(() => {
+    // // setTimeout 中 setState 是同步的
+    // setTimeout(() => {
+    //   this.setState({ count: this.state.count + 1 });
+    //   console.log('count in setTimeout', this.state.count);
+    // })
+
+
+    // 第四，state异步更新的话，更新前会被合并。（类似 Object.assign ）执行结果只一次 +1
+    // this.setState({ count: this.state.count + 1 });
+    // this.setState({ count: this.state.count + 1 });
+    // this.setState({ count: this.state.count + 1 });
+
+
+    // 传入函数不会被合并，函数无法合并，执行结果是 +3
+    // Vue异步更新会过滤
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1
+      }
+    });
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1
+      }
+    });
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1
+      }
+    });
+
+  }
+
+  // componentDidMount() {
+  //   // 自己定义的 DOM 事件，setState 是同步的
+  //   document.body.addEventListener('click', () => {
   //     this.setState({ count: this.state.count + 1 });
-  //     console.log('count in setTimeout', this.state.count);
+  //     console.log('count in body event', this.state.count);
   //   })
   // }
-
-  componentDidMount() {
-    // 自己定义的 DOM 事件，setState 是同步的
-    document.body.addEventListener('click', () => {
-      this.setState({ count: this.state.count + 1 });
-      console.log('count in body event', this.state.count);
-    })
-  }
 }
