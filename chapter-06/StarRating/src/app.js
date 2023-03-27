436bfee9d0c25284d7f6263b101c0aca99eb04b4
何琦 <>
2023-03-27 14:35:08 +0800

    legacy模式 和 concurrent模式下运行例子

--- chapter-06/StarRating/src/app.js
+++ chapter-06/StarRating/src/app.js
@@ -1,22 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ColorList from "./components/ColorList";
 import colorData from "../data/color-data.json";
 
 
+
+class Index extends React.Component {
+  state = { number: 0 }
+  handleClick = () => {
+    setTimeout(() => {
+      this.setState({ number: this.state.number + 1 }, () => {
+        console.log('callback1', this.state.number)
+      })
+      console.log(this.state.number)
+
+      this.setState({ number: this.state.number + 1 }, () => {
+        console.log('callback2', this.state.number)
+      })
+      console.log(this.state.number)
+
+      this.setState({ number: this.state.number + 1 }, () => {
+        console.log('callback3', this.state.number)
+      })
+      console.log(this.state.number)
+    })
+  }
+  render() {
+    return <div>
+      {this.state.number}
+      <button onClick={this.handleClick}> number++ </button>
+    </div>
+  }
+}
+
 export default function App() {
   const [colors, setColors] = useState(colorData);
   return (
-    <ColorList
-      colors={colors}
-      onRateColor={(id, rating) => {
-        const newColors = colors.map(color => color.id === id ? { ...color, rating } : color);
-        setColors(newColors);
-      }}
-      onRemoveColor={id => {
-        const newColors = colors.filter(color => color.id !== id);
-        setColors(newColors);
-      }}
-    />
+    <Index />
+    // <ColorList
+    //   colors={colors}
+    //   onRateColor={(id, rating) => {
+    //     const newColors = colors.map(color => color.id === id ? { ...color, rating } : color);
+    //     setColors(newColors);
+    //   }}
+    //   onRemoveColor={id => {
+    //     const newColors = colors.filter(color => color.id !== id);
+    //     setColors(newColors);
+    //   }}
+    // />
   )
 }
 
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -6,11 +6,40 @@ import store from './app/store';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 
+class Index extends React.Component {
+  state = { number: 0 }
+  handleClick = () => {
+    setTimeout(() => {
+      this.setState({ number: this.state.number + 1 }, () => {
+        console.log('callback1', this.state.number)
+      })
+      console.log(this.state.number)
+
+      this.setState({ number: this.state.number + 1 }, () => {
+        console.log('callback2', this.state.number)
+      })
+      console.log(this.state.number)
+
+      this.setState({ number: this.state.number + 1 }, () => {
+        console.log('callback3', this.state.number)
+      })
+      console.log(this.state.number)
+    })
+  }
+  render() {
+    return <div>
+      {this.state.number}
+      <button onClick={this.handleClick}> number++ </button>
+    </div>
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      {/* <App /> */}
+      <Index />
     </React.StrictMode>
   </Provider>
 );
